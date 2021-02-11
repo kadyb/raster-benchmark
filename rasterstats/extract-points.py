75929b847c404d6b63ee2a3bc2a1310d0377b123
Krzysztof Dyba <35004826+kadyb@users.noreply.github.com>
2021-02-11 11:45:45 +0100

    add 'rasterstats'

---
+++ rasterstats/extract-points.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# skip this test because one band proccesing takes more than 6 min
+if False:
+
+    import os
+    from rasterstats import point_query
+    
+    
+    wd = os.getcwd()
+    catalog = 'data\\LC08_L1TP_190024_20200418_20200822_02_T1'
+    rasters = os.listdir(catalog)
+    rasters = [r for r in rasters if r.endswith(('.TIF'))]
+    rasters = [os.path.join(wd, catalog, r) for r in rasters]
+    band_names = ["B1", "B10", "B11", "B2", "B3", "B4", "B5", "B6", "B7", "B9"]
+    
+    points = 'data\\vector\\points.gpkg'
+    
+    ### extract
+
+    data = []
+    for ras in rasters:
+        vals = point_query(points, ras, interpolate = 'nearest')
+        data.append(vals)
---
+++ rasterstats/zonal.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import os
+import timeit
+import pandas as pd
+from rasterstats import zonal_stats
+
+
+wd = os.getcwd()
+catalog = 'data\\LC08_L1TP_190024_20200418_20200822_02_T1'
+rasters = os.listdir(catalog)
+rasters = [r for r in rasters if r.endswith(('.TIF'))]
+rasters = [os.path.join(wd, catalog, r) for r in rasters]
+band_names = ["B1", "B10", "B11", "B2", "B3", "B4", "B5", "B6", "B7", "B9"]
+
+buffers = 'data\\vector\\buffers.gpkg'
+
+### zonal
+
+t_list = [None] * 10
+for i in range(10):
+    tic = timeit.default_timer()
+    data = []
+    for ras in rasters:
+        zonal = zonal_stats(buffers, ras, stats = 'mean')
+        zonal = [z['mean'] for z in zonal]
+        data.append(zonal)
+    
+    data = pd.DataFrame(data).transpose()
+    data.columns = band_names
+    
+    toc = timeit.default_timer()
+    t_list[i] = round(toc - tic, 2)
+    
+
+df = {'task': ['zonal'] * 10, 'package': ['rasterstats'] * 10, 'time': t_list}
+df = pd.DataFrame.from_dict(df)
+if not os.path.isdir('results'): os.mkdir('results')
+df.to_csv('results\\zonal-rasterstats.csv', index = False, decimal = ',',
+          sep = ';')
