a68976d9c30f11b8ef4aeaee4bdd29795e504267
Krzysztof Dyba <35004826+kadyb@users.noreply.github.com>
2021-04-27 21:38:51 +0200

    use 'along' arg in read_stars

--- stars/crop.R
+++ stars/crop.R
@@ -8,9 +8,7 @@ area = st_bbox(c(xmin = 598500, xmax = 727500,
 rasters = list.files("data/LC08_L1TP_190024_20200418_20200822_02_T1/",
                      pattern = "\\.TIF$", full.names = TRUE)
 
-ras = read_stars(rasters, proxy = FALSE)
-ras = st_redimension(ras)
-
+ras = read_stars(rasters, along = 3, proxy = FALSE)
 ras_names = c("B1", "B10", "B11", "B2", "B3", "B4", "B5", "B6", "B7", "B9")
 ras = st_set_dimensions(ras, 3, values = ras_names, names = "band")
 
