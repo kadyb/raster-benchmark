925d59436474b3dda004b4734bb3c003f96396f5
Krzysztof Dyba <35004826+kadyb@users.noreply.github.com>
2021-05-08 21:45:22 +0200

    load all data into memory in 'terra'

--- terra/crop.R
+++ terra/crop.R
@@ -7,6 +7,7 @@ rasters = list.files("data/LC08_L1TP_190024_20200418_20200822_02_T1/",
                      pattern = "\\.TIF$", full.names = TRUE)
 
 ras = rast(rasters)
+ras = ras * 1 # explicitly load into memory
 
 ras_names = c("B1", "B10", "B11", "B2", "B3", "B4", "B5", "B6", "B7", "B9")
 names(ras) = ras_names
