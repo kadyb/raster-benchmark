fbf268857702d225c84323159d732e99375602c1
Krzysztof Dyba <35004826+kadyb@users.noreply.github.com>
2022-10-19 15:00:36 +0200

    faster data loading in `{rasterio}`

--- rasterio/load.py
+++ rasterio/load.py
@@ -2,6 +2,7 @@
 import os
 import timeit
 import rasterio
+import numpy as np
 import pandas as pd
 
 
@@ -12,37 +13,24 @@ rasters = [r for r in rasters if r.endswith(('.TIF'))]
 rasters = [os.path.join(wd, catalog, r) for r in rasters]
 
 
-### raster stack
-
-band_names = ["B1", "B10", "B11", "B2", "B3", "B4", "B5", "B6", "B7", "B9"]
-
-with rasterio.open(rasters[0]) as ras:
-    meta = ras.meta
-
-meta.update(count = len(rasters))
-
-stack_file = 'stack.TIF'
-# source rasters have 'DEFLATE' compression
-with rasterio.open(stack_file, 'w', **meta, compress = 'DEFLATE') as dst:
-    for id, layer in enumerate(rasters, start = 1):
-        with rasterio.open(layer) as stack:
-            dst.write_band(id, stack.read(1))
-            dst.set_band_description(id, band_names[id - 1])
-
-### load
-
 t_list = [None] * 10
 for i in range(10):
     tic = timeit.default_timer()
 
-    ras = rasterio.open('stack.TIF')
-    data = ras.read(masked = True)
-    ras.close()
+    ls = []
+    for r in rasters:
+        src = rasterio.open(r, "r")
+        meta = src.meta
+        array = src.read(masked = True).squeeze()
+        ls.append(array)
+        src.close()
+    
+    ras = np.ma.asarray(ls, dtype = 'float')
+    np.ma.set_fill_value(ras, meta['nodata'])
+    meta.update(count = len(rasters))
     
     toc = timeit.default_timer()
     t_list[i] = round(toc - tic, 2)
-    
-os.remove(stack_file)
 
 
 df = {'task': ['load'] * 10, 'package': ['rasterio'] * 10, 'time': t_list}
