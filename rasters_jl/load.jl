a8abb00779bdc2651faf1ee327b88b0c5ae64c90
Anshul Singhvi <asinghvi17@simons-rock.edu>
2024-09-24 15:12:08 -0700

    Make `load` eager
    conforming with all the other benchmarks

--- rasters_jl/load.jl
+++ rasters_jl/load.jl
@@ -10,7 +10,8 @@ points_df = GeoDataFrames.read(joinpath(data_dir, "vector", "points.gpkg"))
 raster_dir = joinpath(data_dir, "LC08_L1TP_190024_20200418_20200822_02_T1")
 raster_files = filter(endswith(".TIF"), readdir(raster_dir; join=true))
 
+Rasters.checkmem!(false) # make sure that Rasters does not error because it thinks there isn't enough memory
 # do something
-benchmark = @be RasterStack($raster_files; lazy=true) seconds=10
+benchmark = @be RasterStack($raster_files) seconds=10
 
 write_benchmark_as_csv(benchmark; task="load")
