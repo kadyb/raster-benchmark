00806cdbb74db1667462f00ad0e8c2efadf4248a
rafaqz <rafaelschouten@gmail.com>
2024-09-25 01:55:33 +0200

    bugfix nvdi

--- rasters_jl/ndvi.jl
+++ rasters_jl/ndvi.jl
@@ -9,16 +9,12 @@ points_df = GeoDataFrames.read(joinpath(data_dir, "vector", "points.gpkg"))
 
 raster_dir = joinpath(data_dir, "LC08_L1TP_190024_20200418_20200822_02_T1")
 raster_files = filter(endswith(".TIF"), readdir(raster_dir; join=true))
-rasters = Raster.(raster_files; lazy=true)
+# TODO: replace_missing will not be needed in the next Rasters version
+red = replace_missing(Raster(raster_files[5]))
+nir = replace_missing(Raster(raster_files[6]))
 
 # do something
 get_ndvi(red, nir) = (nir .- red) ./ (nir .+ red)
-
-Rasters.checkmem!(false)
-
-# read moves the data to memory - or the function will be lazy and instant!
-red = read(rasters[5])
-nir = read(rasters[6])
 
 benchmark = @be get_ndvi($red, $nir) seconds=15
 
