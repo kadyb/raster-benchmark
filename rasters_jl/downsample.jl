4a3072615aa5d67e8e4667f2565787f8d2639b5b
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-02-04 14:34:45 -0500

    use `aggregate` (native Julia) instead of `resample` (GDAL)

--- rasters_jl/downsample.jl
+++ rasters_jl/downsample.jl
@@ -14,6 +14,9 @@ band_names = (:B1, :B10, :B11, :B2, :B3, :B4, :B5, :B6, :B7, :B9)
 Rasters.checkmem!(false) # Avoid memory check bug on some machines
 raster = Raster(RasterStack(raster_files; name=band_names))
 # Downsample from 30m to 90m (1/3 of the original resolution)
-benchmark = @be Rasters.resample($raster; res=90) seconds=60
+benchmark = @be Rasters.aggregate($sum, $raster, $(3)) seconds=60
+
+# The other way to do this (via GDAL) is:
+# benchmark = @be Rasters.resample($raster; res=90) seconds=60
 
 write_benchmark_as_csv(benchmark; task = "downsample")
