26816b154f20c9bf19e0161e1ab49e24f130de8b
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-02-04 14:35:17 -0500

    use LZW compression in `write`

--- rasters_jl/write.jl
+++ rasters_jl/write.jl
@@ -14,6 +14,10 @@ band_names = (:B1, :B10, :B11, :B2, :B3, :B4, :B5, :B6, :B7, :B9)
 Rasters.checkmem!(false) # avoid a memory size bug on some machines
 ras = Raster(RasterStack(raster_files; name=band_names))
 stack_file = joinpath(data_dir, "stack.tif")
-benchmark = @be write($stack_file, $ras; force=true) seconds=60
+# note that we use force=true to force overwriting the file,
+# and Rasters compresses by Zstd by default, but every other 
+# package uses LZW - so for consistency's sake we force the compression
+# to LZW as well.
+benchmark = @be write($stack_file, $ras; force=$true, options = $(Dict("COMPRESS" => "LZW"))) seconds=60
 
 write_benchmark_as_csv(benchmark; task = "write")
